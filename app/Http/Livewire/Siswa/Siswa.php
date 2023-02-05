bf608df855b2577a288897adc7661336253bc892
adimalikibrahim <adimalikibrahim@gmail.com>
2023-02-06 06:49:19 +0700

    search

--- app/Http/Livewire/Siswa/Siswa.php
+++ app/Http/Livewire/Siswa/Siswa.php
@@ -43,10 +43,16 @@ class Siswa extends Component
         // $json = Http::withToken('WLdZ6LQ9CD7plYq')->get('http://localhost:5774/WebService/getPesertaDidik?npsn=20263295');
         $json = Http::get('https://dummyjson.com/users');
         $data1 = $json->json('users');
+        $s = collect($data1)->where(function($query){
+            $query->where('name', 'like', '%'.$this->search.'%')
+                ->orWhere('username', 'like', '%'.$this->search.'%')
+                ->orWhere('email', 'like', '%'.$this->search.'%');
+            });
         // dd($json);
         return view('livewire.siswa.siswa', [
             'data' => $this->paginate($data1),
             'total' => $json->json('limit'),
+            's' => $s
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -9,38 +9,35 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', Dashboard::class);
+Route::get('/siswa', Siswa::class);
+
 Route::group(['middleware' => ['auth', 'role:admin && user', 'verified']], function () {
-    // Route::get('/page2', Page2::class);
     Route::get('/profile', Profile::class)->name('profile');
 });
 
 Route::group(['middleware' => ['auth', 'role:admin', 'verified']], function () {
-    Route::get('/siswa', Siswa::class);
     Route::get('/users', User::class);
+});
+
+
     // -----------------------------------------------------------------------------------------------------------------
     //  Profile related routes
     // -----------------------------------------------------------------------------------------------------------------
 
     // Route::get('/profile', [\App\Http\Controllers\ProfileController::class, 'editProfile'])
     //     ->name('profile');
-
     // Route::post('/profile/avatar', [\App\Http\Controllers\ProfileController::class, 'updateAvatar'])
     //     ->name('profile.avatar');
-
     // Route::delete('/profile/avatar', [\App\Http\Controllers\ProfileController::class, 'removeOldAvatar'])
     //     ->name('profile.deleteavatar');
-
     // Route::delete('/profile/device/{id}', [\App\Http\Controllers\ProfileController::class, 'removeDevice'])
     //     ->name('profile.deletedevice');
-
     // Route::get('/profile/theme', function (\Illuminate\Http\Request $request) {
     //     if ($request->has('theme')) {
     //         (new \App\Actions\UserSettings)->set('user_theme', 'theme-'.$request->theme);
-
     //         return redirect()->back();
     //     }
     // })->name('profile.theme');
-
 
     // -----------------------------------------------------------------------------------------------------------------
     //  User management related routes
@@ -48,29 +45,19 @@ Route::group(['middleware' => ['auth', 'role:admin', 'verified']], function () {
 
     // Route::get('/users', [\App\Http\Controllers\UsersController::class, 'index'])
     //     ->name('users.index');
-
     // Route::get('/user/create', [\App\Http\Controllers\UsersController::class, 'create'])
     //     ->name('users.create');
-
     // Route::post('/user/store', [\App\Http\Controllers\UsersController::class, 'store'])
     //     ->name('users.store');
-
     // Route::get('/user/{user}/edit', [\App\Http\Controllers\UsersController::class, 'edit'])
     //     ->name('users.edit');
-
     // Route::put('/user/{user}/update', [\App\Http\Controllers\UsersController::class, 'update'])
     //     ->name('users.update');
-
     // Route::get('/user/{user}/change-password', [\App\Http\Controllers\UsersController::class, 'changePassword'])
     //     ->name('users.change-password');
-
     // Route::put('/user/{user}/update-password', [\App\Http\Controllers\UsersController::class, 'updatePassword'])
     //     ->name('users.update-password');
-
     // Route::delete('/user/{user}/delete', [\App\Http\Controllers\UsersController::class, 'delete'])
     //     ->name('users.delete');
-
     // Route::get('/user/{user}/delete-avatar', [\App\Http\Controllers\UsersController::class, 'deleteAvatar'])
     //     ->name('users.delete-avatar');
-
-});
